60e0e57460dff78c166b738c8b5c982b47618fab
unknown <90293855+fishdestroyer@users.noreply.github.com>
2021-10-10 22:11:58 -0500

    10-10-21-2nd commit

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,3 +1,10 @@
+// Game States
+// "WIN" - Player robot has defeated all enemy-robots
+//    * Fight all enemy-robots
+//    * Defeat each enemy-robot
+// "LOSE" - Player robot's health is zero or less
+
+
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
@@ -7,7 +14,8 @@ var playerMoney = 10;
 // Logged playerMoney var
 console.log(playerName, playerAttack, playerHealth, playerMoney);
 //
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
+console.log(enemyNames);
 var enemyHealth = 100;
 var enemyAttack = 12;
 
@@ -73,7 +81,4 @@ else {
 }
 
 
-// var playerName = "Tony the Robot"
-// console.log(playerName + "is ready for battle")
-
-fight();
+//fight();
